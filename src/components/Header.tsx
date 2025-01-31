8b5b7fa6352990d2ace3025d5d32341b9eab70e1
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-01-31 22:32:42 +0700

    create component header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,27 @@
 const Header = () => {
-    return (
-      <header className="relative">
-        <div className="relative">
-          <img
-            src="/images/wedding/img16.jpeg"
-            alt="Header Background"
-            className="w-full h-[60vh] object-cover"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+  return (
+    <div
+      className="relative w-full h-screen bg-cover bg-center flex justify-center items-center"
+      style={{ backgroundImage: "url('/images/wedding/header.jpeg')" }}
+    >
+      {/* Wrapper agar tetap seukuran Mobile L (425px) dan berada di tengah */}
+      <div className="relative w-full bg-black bg-opacity-30 flex flex-col justify-end text-white h-full">
+        <div className="ml-3">
+          <h2 className="text-sm md:text-4xl font1">The Wedding of</h2>
+          <h1 className="text-2xl md:text-5xl font2 mt-2 mb-3 underline underline-offset-8">WISNU & METLANI</h1>
+          <p className="text-sm md:text-xl mt-4 font1">18 Februari 2025</p>
         </div>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+        {/* Ornamental Decoration */}
+        <div className="w-full mt-4 overflow-hidden">
+          <img
+            src="/images/ornamen/ornamen1.png"
+            alt="Decoration"
+            className="w-full max-w-[425px] h-auto object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
