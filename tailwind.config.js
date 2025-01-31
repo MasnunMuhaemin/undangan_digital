65dd5710da42813728bf388dbfa3e182890e806a
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-01-31 19:49:37 +0700

    new configure package

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#322501",
+        tertiary: "#FFFFFF"
+      }
+    },
   },
   plugins: [],
 }
