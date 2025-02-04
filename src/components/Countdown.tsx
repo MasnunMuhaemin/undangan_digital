82a411e7779d0bd3e5b88e580f502332030bd566
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-02-04 23:44:12 +0700

    some error

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,16 +4,16 @@ const WeddingCountdown = () => {
   let targetDate = new Date("2025-02-18T09:00:00+07:00").toISOString();
 
   return (
-    <div className="relative flex flex-col justify-center items-center mobile-420:px-4">
+    <div className="relative flex flex-col justify-center items-center px-4">
       {/* Ornamen (Tetap di atas bg-secondary) */}
       <img
         src="/images/ornamen/ornamen5.png"
         alt="Ornament"
-        className="absolute top-0 left-0 w-full h-auto min-h-screen object-contain z-0"
+        className="absolute top-0 left-0 w-full h-auto object-contain z-0"
       />
 
       {/* Content Section */}
-      <div className="relative z-20 text-white text-center mobile-320:mt-0 mobile-425:mt-20">
+      <div className="relative z-20 text-white text-center mobile-320:mt-10 mt-20">
         <h1 className="font1 mobile-320:text-[10px] mobile-425:text-lg font-bold">HARI YANG KAMI NANTI</h1>
         <p className="font2 mobile-320:text-[8px] mobile-320:w-[240px] mobile-375:text-[12px] mobile-375:w-[270px] text-sm w-[300px] font-bold mt-3 mobile-320:mb-0 mobile-375:mb-4 leading-relaxed">
           Dengan segala rasa syukur yang tak terhingga, kami mengucap bahagia.
